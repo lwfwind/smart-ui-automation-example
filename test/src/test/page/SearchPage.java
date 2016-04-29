b06f9430b425ed64786adc674b647596ba6d197c
wind <wind@abc360.com>
2016-04-29 15:32:51 +0800

    initian

---
+++ test/src/test/page/SearchPage.java
@@ -0,0 +1,23 @@
+package test.page;
+
+import com.qa.framework.PageBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+public class SearchPage extends PageBase {
+
+    @FindBy(id = "kw")
+    private WebElement searchTestBox;
+
+    public void searchFor(String text) {
+        driver.get("http://www.baidu.com/");
+        searchTestBox.sendKeys(text);
+        searchTestBox.submit();
+    }
+
+    public void verifyResult() {
+        Assert.assertTrue(getPageTitle().contains("百度搜索"));
+    }
+
+}
