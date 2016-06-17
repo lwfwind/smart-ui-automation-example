78326e7265da4476394563e34afde06d1692ad1d
wind <wind@abc360.com>
2016-06-17 17:08:36 +0800

    support modify config from pom

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ﻿Example for [smart-ui-framework](https://github.com/lwfwind/smart-ui-framework)
 
 ## Run
-    mkdir test-xml
     mvn clean & mvn test -P dev
 
 
--- src/test/page/SearchPage.java
+++ src/test/page/SearchPage.java
@@ -1,17 +1,19 @@
 package test.page;
 
 import com.qa.framework.PageBase;
+import com.qa.framework.config.PropConfig;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
 public class SearchPage extends PageBase {
 
+    String url = PropConfig.getWebPath();
     @FindBy(id = "kw")
     public WebElement searchTestBox;
 
     public void searchFor(String text) {
-        open("http://www.baidu.com/");
+        open(url);
         searchTestBox.sendKeys(text);
         searchTestBox.submit();
     }
